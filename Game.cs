76b019811016d0a4ff6f47764a51e8ced56e5321
Jacob Stilin <stilinj@gmail.com>
2019-09-25 14:12:15 -0500

    Added game settings and a setup class

--- Game.cs
+++ Game.cs
@@ -9,57 +9,25 @@ namespace RPSLS
     class Game
     {
         //member variables
-        int round;
+        
         int player1Score;
         int player2Score;
         int bestOf;
-        bool pvpSelect;
-        string pvp;
+        Setup setup;
 
 
         //constructor spawner
         public Game()
         {
-            round = 1;
-            bestOf = 0;
+            setup = new Setup();
 
         }
         //member methods
         public void StartGame()
         {
-            pvpSelect = PvPSelector();
-            Console.WriteLine(pvpSelect);
-            bestOf = BestOf();
-            Console.ReadLine();
-;
+            setup.StartGame();
         }
-        public bool PvPSelector()
-        {
-            Console.WriteLine("Is this PvP or PvE?");
-            pvp = Console.ReadLine();
-            if(pvp == "pvp")
-            {
-                return true;
-            }
-            if(pvp == "pve")
-            {
-                return false;
-            }
-            else
-            {
-                return PvPSelector();
-            }
-        }
-
-        public int BestOf()
-        {
-            Console.WriteLine("Best of how many rounds?");
-            int input = Console.Read();
-            
-                return input;
-            
-            
-        }
+        
         
     }
 }
